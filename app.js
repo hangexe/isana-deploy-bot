3ddd88c379ef7570d43618d3890076dbfe42e46f
Ung Nhat Hang <hang.ung@executionlab.asia>
2021-02-02 11:30:49 +0700

    update

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
-var express = require('express');
-var path = require('path');
-// var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var logger = require("morgan");
+var bodyParser = require("body-parser");
 
 var app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger("dev"));
+app.use(bodyParser.json({type: 'application/*+json'}))
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get("/api/deploy-isana-android", (req, res, next) => {
-  res.json({message: "OK"})
-})
+
+app.post("/api/deploy-isana-android", (req, res, next) => {
+  console.log(req.params['challenge'])
+  console.log(req.body)
+  const {challenge} = req.body
+  // const {challenge} = req.params;
+
+  // logger
+  res.json({
+    challenge,
+  });
+  // body
+  // {
+  //   "token": "Jhj5dZrVaK7ZwHHjRyZWjbDl",
+  //   "challenge": "3eZbrw1aBm2rZgRNFdxV2595E9CY3gmdALWMmHkvFXO7tYXAYM8P",
+  //   "type": "url_verification"
+  // }
+});
 
 module.exports = app;
