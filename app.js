6840f2f99171ecd10bd2dff0c794b8c55a8fd593
Ung Nhat Hang <hang.ung@executionlab.asia>
2021-02-02 13:07:08 +0700

    Heroku Exec

--- app.js
+++ app.js
@@ -6,26 +6,19 @@ var bodyParser = require("body-parser");
 var app = express();
 
 app.use(logger("dev"));
-app.use(bodyParser.json({type: 'application/*+json'}))
-app.use(bodyParser.urlencoded({ extended: false }))
-
+app.use(bodyParser.json({ type: "application/*+json" }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post("/api/deploy-isana-android", (req, res, next) => {
-  console.log(req.params['challenge'])
-  console.log(req.body)
-  const {challenge} = req.body
+  console.log(req.params["challenge"]);
+  const { body } = req;
+  const { challenge } = req.body;
   // const {challenge} = req.params;
 
   // logger
   res.json({
     challenge,
   });
-  // body
-  // {
-  //   "token": "Jhj5dZrVaK7ZwHHjRyZWjbDl",
-  //   "challenge": "3eZbrw1aBm2rZgRNFdxV2595E9CY3gmdALWMmHkvFXO7tYXAYM8P",
-  //   "type": "url_verification"
-  // }
 });
 
 module.exports = app;
