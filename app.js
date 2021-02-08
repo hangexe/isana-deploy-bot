a57bcc14edf4a552ac88f9565a41a440fadd6c7e
Ung Nhat Hang <hang.ung@executionlab.asia>
2021-02-08 11:21:03 +0700

    procfile

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var express = require("express");
 var path = require("path");
 var logger = require("morgan");
 var bodyParser = require("body-parser");
-const { json } = require("body-parser");
 
 var app = express();
 
@@ -10,16 +9,18 @@ app.use(logger("dev"));
 app.use(bodyParser.json({ type: "application/json" }));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/", (req,res,next) => {
-  res.end(`<h1 style="margin: 200px auto">Hello, welcome to Slackbot of Isana team</h1>`)
-})
+app.get("/", (req, res, next) => {
+  res.end(
+    `<h1 style="margin: 200px auto">Hello, welcome to Slackbot of Isana team</h1>`
+  );
+});
 
 app.post("/api/deploy-isana-android", (req, res, next) => {
   const { body } = req;
-  console.log({body})
+  console.log({ body });
   res.json({
     message: "success",
-    body
+    body,
   });
 });
 
