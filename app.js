4b955a947a6b96e84dc8f63ae8060e156954c3f8
Ung Nhat Hang <hang.ung@executionlab.asia>
2021-02-08 09:46:54 +0700

    add get route

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ var express = require("express");
 var path = require("path");
 var logger = require("morgan");
 var bodyParser = require("body-parser");
+const { json } = require("body-parser");
 
 var app = express();
 
 app.use(logger("dev"));
 app.use(bodyParser.json({ type: "application/json" }));
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (req,res,next) => {
+  json.end(`<h1 style="margin: 200px auto">Hello, welcome to Slackbot of Isana team</h1>`)
+})
 
 app.post("/api/deploy-isana-android", (req, res, next) => {
   const { body } = req;
