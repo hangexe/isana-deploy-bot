0635d76ce144e6fba1c99eda268530c225941e91
Ung Nhat Hang <hang.ung@executionlab.asia>
2021-02-08 16:00:51 +0700

    s

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require("path");
 var logger = require("morgan");
 var bodyParser = require("body-parser");
 var fetch = require("node-fetch");
+var { exe } = require("child_process");
 
 var app = express();
 
@@ -20,16 +21,23 @@ app.post("/api/deploy-isana-android", async (req, res, next) => {
   const { body } = req;
   console.log({ body });
 
-  const curlReponse = async () => {
-    try {
-      let jsonRes = await fetch("https://jsonplaceholder.typicode.com/users");
-      return await jsonRes.json();
-    } catch (err) {
-      console.log(err);
-    }
-  };
+  // const curlReponse = async () => {
+  //   try {
+  //     let jsonRes = await fetch("https://jsonplaceholder.typicode.com/users");
+  //     return await jsonRes.json();
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // };
 
-  console.log(curlReponse);
+  try {
+    const output = await exe(
+      "curl -i -u nguyenxuantien3105:b9cb102fd03a6b743d9a34822bdafce50898b25a https://api.github.com/repos/Lighthouse-Inc/isana-android/branches/master"
+    );
+    console.log(output);
+  } catch (err) {
+    console.log(err);
+  }
 
   res.json({
     message: "success",
